daf69ee4cff22d4c7abce9611a5e7bea44308844
S2009 <contact@s2009.fr>
2025-06-18 13:59:29 +0200

    feat(players): :fire: add players movements

---
+++ scripts/entities/players/player_1.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var speed = 400 # Player's Speed (px/s)
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # Player's movement vector
+
+	if Input.is_action_pressed("player1_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("player1_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("player1_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("player1_right"):
+		velocity.x += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$Sprite2D.rotation = atan2(velocity.y, velocity.x) + 90 * PI / 180
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
