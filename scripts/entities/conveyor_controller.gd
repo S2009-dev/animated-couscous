e64fc5218cfb7cb6cd35103ad73fcfb26195c744
S2009 <contact@s2009.fr>
2025-06-19 16:50:21 +0200

    feat: :alembic: add multi-items support to conveyor

--- scripts/entities/conveyor_controller.gd
+++ scripts/entities/conveyor_controller.gd
@@ -1,13 +1,28 @@
 extends TileMapLayer
 
 @export var item_object: PackedScene # Items scene
-@export var items_name: PackedStringArray # What items this conveyor carries
+@export var items_name: PackedStringArray # What items this conveyor carries (not implemented yet)
 @export var speed: int = 8
 
-func _ready() -> void:
-	var item = item_object.instantiate()
-	$Items.add_child(item)
-
 func _process(delta: float) -> void:
-	$Path2D/PathFollow2D.progress_ratio += delta * speed / 100
-	$Items.get_child(0).position = $Path2D/PathFollow2D.position
+    for i in range($Items.get_child_count()):
+        var item = $Items.get_child(i)
+        var path_follow = $Path2D.get_child(i)
+
+        path_follow.progress_ratio += delta * speed / 100
+        item.position = path_follow.position
+
+        if path_follow.progress_ratio >= 1:
+            item.queue_free()
+            path_follow.queue_free()
+
+
+func _on_item_spawn_interval_timeout() -> void:
+    var item = item_object.instantiate()
+    var path_follow = PathFollow2D.new()
+
+    item.position = path_follow.position
+    path_follow.loop = false
+
+    $Items.add_child(item)
+    $Path2D.add_child(path_follow)
