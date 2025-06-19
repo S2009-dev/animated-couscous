490b43cf15be841229a14166319bd69af44f7b99
S2009 <contact@s2009.fr>
2025-06-19 15:47:33 +0200

    feat: :sparkles: add conveyor item

--- scripts/entities/conveyor_controller.gd
+++ scripts/entities/conveyor_controller.gd
@@ -1,5 +1,13 @@
 extends TileMapLayer
 
-@export var items_object: PackedScene # Items scene
+@export var item_object: PackedScene # Items scene
 @export var items_name: PackedStringArray # What items this conveyor carries
+@export var speed: int = 8
 
+func _ready() -> void:
+	var item = item_object.instantiate()
+	$Items.add_child(item)
+
+func _process(delta: float) -> void:
+	$Path2D/PathFollow2D.progress_ratio += delta * speed / 100
+	$Items.get_child(0).position = $Path2D/PathFollow2D.position
