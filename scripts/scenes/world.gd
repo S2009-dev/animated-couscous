31c614b14ddb1a18bcc8a5cf0f0a299704ac4487
S2009 <contact@s2009.fr>
2025-06-21 11:02:05 +0200

    refactor(project): :construction: update structure in order to have a working split-screen
    BREAKING CHANGES

--- scripts/scenes/world.gd
+++ scripts/scenes/world.gd
@@ -5,8 +5,8 @@ signal world_loaded
 @export var player_object: PackedScene
 
 func _ready() -> void:
-	create_player(1, Vector2(100, 100))
-	create_player(2, Vector2(200, 200))
+	create_player(1, Vector2(-600, 270))
+	create_player(2, Vector2(600, 270))
 	emit_signal("world_loaded")
 
 func create_player(id: int, start_pos: Vector2 = position) -> void:
@@ -15,4 +15,4 @@ func create_player(id: int, start_pos: Vector2 = position) -> void:
 	player.id = clampi(id, 1, 2)
 	player.position = start_pos
 
-	$Players.add_child(player)
+	$Cameras/SubViewportContainer/Viewport1/Players.add_child(player)
