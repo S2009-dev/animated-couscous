67a7db22bff409b7f51204f716a2fdc377c6935b
S2009 <contact@s2009.fr>
2025-06-23 13:22:18 +0200

    feat: :sparkles: options saving

--- scripts/menus/title_screen.gd
+++ scripts/menus/title_screen.gd
@@ -1,10 +1,19 @@
 extends Control
 
 var current_menu: String = "main"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var config: ConfigFile = ConfigFile.new()
 
+func _ready() -> void:
+	var conf_load = config.load("user://options.cfg")
+	var volume = 50
+
+	if conf_load == OK and config.has_section_key("settings", "volume"):
+		volume = config.get_value("settings", "volume")
+	
+	AudioServer.set_bus_volume_db(0, linear_to_db(volume / 100))
+	$OptionsContainer/Volume/Slider.value = volume
+
+	save_options()
 
 func _on_options_btn_pressed() -> void:
 	current_menu = "options"
@@ -14,14 +23,12 @@ func _on_options_btn_pressed() -> void:
 	$OptionsContainer.show()
 	$BackBtn.show()
 
-
 func _on_credits_btn_pressed() -> void:
 	current_menu = "credits"
 
 	$Title.text = "CREDITS"
 	$MainContainer.hide()
 	$BackBtn.show()
-
 
 func _on_back_btn_pressed() -> void:
 	if current_menu == "options":
@@ -35,11 +42,16 @@ func _on_back_btn_pressed() -> void:
 	$MainContainer.show()
 	$BackBtn.hide()
 
-
 func _on_slider_drag_ended(value_changed: bool) -> void:
 	if value_changed:
 		AudioServer.set_bus_volume_db(0, linear_to_db($OptionsContainer/Volume/Slider.value / 100))
-
+		save_options()
 
 func _on_slider_value_changed(value: float) -> void:
 	$OptionsContainer/Volume/Label.text = "VOLUME: " + str(roundi(value)) + "%"
+
+func save_options() -> void:
+	var volume = $OptionsContainer/Volume/Slider.value
+	
+	config.set_value("settings", "volume", volume)
+	config.save("user://options.cfg")
