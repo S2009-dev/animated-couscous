889b786082bb2a2d160579fee87efb5b344de5b9
S2009 <contact@s2009.fr>
2025-06-23 11:51:12 +0200

    feat: :sparkles: add back button

--- scripts/menus/title_screen.gd
+++ scripts/menus/title_screen.gd
@@ -1,14 +1,28 @@
 extends Control
 
-
+var current_menu: String = "main"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _on_options_btn_pressed() -> void:
+	current_menu = "options"
+
 	$Title.text = "OPTIONS"
+	$MainContainer.hide()
+	$BackBtn.show()
 
 
 func _on_credits_btn_pressed() -> void:
+	current_menu = "credits"
+
 	$Title.text = "CREDITS"
+	$MainContainer.hide()
+	$BackBtn.show()
+
+
+func _on_back_btn_pressed() -> void:
+	$Title.text = "ANIMATED-COUSCOUS"
+	$MainContainer.show()
+	$BackBtn.hide()
