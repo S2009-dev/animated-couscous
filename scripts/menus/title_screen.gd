8df048b995d3616737c9e443b47744ee6978e221
S2009 <contact@s2009.fr>
2025-06-23 13:05:11 +0200

    feat: :sparkles: add volume option

--- scripts/menus/title_screen.gd
+++ scripts/menus/title_screen.gd
@@ -11,6 +11,7 @@ func _on_options_btn_pressed() -> void:
 
 	$Title.text = "OPTIONS"
 	$MainContainer.hide()
+	$OptionsContainer.show()
 	$BackBtn.show()
 
 
@@ -23,6 +24,22 @@ func _on_credits_btn_pressed() -> void:
 
 
 func _on_back_btn_pressed() -> void:
+	if current_menu == "options":
+		$OptionsContainer.hide()
+	elif current_menu == "credits":
+		pass
+
+	current_menu = "main"
+
 	$Title.text = "ANIMATED-COUSCOUS"
 	$MainContainer.show()
 	$BackBtn.hide()
+
+
+func _on_slider_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		AudioServer.set_bus_volume_db(0, linear_to_db($OptionsContainer/Volume/Slider.value / 100))
+
+
+func _on_slider_value_changed(value: float) -> void:
+	$OptionsContainer/Volume/Label.text = "VOLUME: " + str(roundi(value)) + "%"
