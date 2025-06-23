4bdaa085f325e0c20b876cbea0da34e23219833d
S2009 <contact@s2009.fr>
2025-06-23 11:43:31 +0200

    test: :poop: buttons action

--- scripts/menus/title_screen.gd
+++ scripts/menus/title_screen.gd
@@ -6,14 +6,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_options_btn_pressed() -> void:
-	pass # Replace with function body.
+	$Title.text = "OPTIONS"
 
 
 func _on_credits_btn_pressed() -> void:
-	pass # Replace with function body.
+	$Title.text = "CREDITS"
