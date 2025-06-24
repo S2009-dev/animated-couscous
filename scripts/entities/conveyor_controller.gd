1bcf341d9a08b679daa4398537a4f5bee1f03e9f
S2009 <contact@s2009.fr>
2025-06-24 11:49:43 +0200

    refactor(environment): :recycle: use unique names and add a unique ID to each item and path in conveyor_controller script

--- scripts/entities/conveyor_controller.gd
+++ scripts/entities/conveyor_controller.gd
@@ -3,26 +3,33 @@ extends TileMapLayer
 @export var item_object: PackedScene # Items scene
 @export var speed: int = 8
 
+@onready var paths: Path2D = %Path2D
+@onready var items: Node2D = %Items
+
 func _process(delta: float) -> void:
-    for i in range($Items.get_child_count()):
-        var item = $Items.get_child(i)
-        var path_follow = $Path2D.get_child(i)
+	for i in range(items.get_child_count()):
+		var item = items.get_child(i)
+		var path_follow = paths.get_child(i)
 
-        path_follow.progress_ratio += delta * speed / 100
-        item.position = path_follow.position
+		path_follow.progress_ratio += delta * speed / 100
+		item.position = path_follow.position
 
-        if path_follow.progress_ratio >= 1:
-            item.queue_free()
-            path_follow.queue_free()
+		if path_follow.progress_ratio >= 1:
+			item.queue_free()
+			path_follow.queue_free()
 
 
 func _on_item_spawn_interval_timeout() -> void:
-    var item = item_object.instantiate()
-    var path_follow = PathFollow2D.new()
+	var item = item_object.instantiate()
+	var path_follow = PathFollow2D.new()
+	var id = items.get_child_count() + 1
+	
+	item.name = "Item" + str(id)
+	item.position = path_follow.position
 
-    item.position = path_follow.position
-    path_follow.loop = false
-    path_follow.add_to_group("items_path")
+	path_follow.name = "PathFollow" + str(id)
+	path_follow.loop = false
+	path_follow.add_to_group("items_path")
 
-    $Items.add_child(item)
-    $Path2D.add_child(path_follow)
+	items.add_child(item)
+	paths.add_child(path_follow)
