6400418d626026d568207c84df0d8f8ca49dd0df
S2009 <contact@s2009.fr>
2025-06-25 15:11:38 +0200

    feat: :sparkles: add hud_manager script

---
+++ scripts/scenes/hud_manager.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var player1_item: ColorRect = %Player1Item
+@onready var player2_item: ColorRect = %Player2Item
+@onready var best_score_label: Label = %BestScore
+@onready var score_label: Label = %Score
+@onready var game_over: Control = %GameOver
+
+var score: int = 0
+var best_score: int = 0
+var config: ConfigFile = ConfigFile.new()
+
+func _ready() -> void:
+	var conf_load = config.load("user://scores.cfg")
+
+	if conf_load == OK and config.has_section_key("scores", "best_score"):
+		best_score = config.get_value("scores", "best_score")
+		best_score_label.text = "BEST SCORE: " + str(best_score)
+
+	config.set_value("scores", "best_score", best_score)
+	config.save("user://best_score.cfg")
+
+func _on_update_score() -> void:
+	score += 1
+	score_label.text = str(score)
+
+	if score > best_score:
+		update_best_score()
+
+func update_best_score() -> void:
+	best_score = score
+	best_score_label.text = "BEST SCORE: " + str(best_score)
+	
+	config.set_value("scores", "best_score", best_score)
+	config.save("user://best_score.cfg")
