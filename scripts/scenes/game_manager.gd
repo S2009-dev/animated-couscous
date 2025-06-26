ec7c6c8862c8bbf5ed9767881970d7c3e6e3adba
S2009 <contact@s2009.fr>
2025-06-26 15:10:48 +0200

    feat: :zap: add fullscreen toggle key

--- scripts/scenes/game_manager.gd
+++ scripts/scenes/game_manager.gd
@@ -14,6 +14,12 @@ func _ready():
 	add_child(sfx_player)
 	_main()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		var mode := DisplayServer.window_get_mode()
+		var is_window: bool = mode != DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
+
 func _main() -> void:
 	music_player.stop()
 
